6ab92c8bb41cf0610769e510f116313bdd8c0e30
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-04-07 17:05:52 -0500

    JSTL <c:forEach> - Desplegar una lista List<String>

--- src/main/java/alexlondon07/github/io/app/controller/HomeController.java
+++ src/main/java/alexlondon07/github/io/app/controller/HomeController.java
@@ -1,5 +1,8 @@
 package alexlondon07.github.io.app.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,20 @@ public class HomeController {
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String goHome() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String goMain(Model model) {
+		
+		List<String> movies = new LinkedList<>();
+		movies.add("Rapidos y furiosos");
+		movies.add("El aro 2");
+		movies.add("Aliens");
+		movies.add("La purga");
+		
+		model.addAttribute("movies", movies);
+		
 		return "home";
 	}
 	
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Home</title>
+<title>Welcome to cinema app</title>
 </head>
 <body>
 	<h1>Home, Welcome to main page</h1>
+	<ol>
+		<c:forEach items="${movies}" var="movie">
+			<li>${ movie }</li>
+		</c:forEach>
+	</ol>
 </body>
 </html>
