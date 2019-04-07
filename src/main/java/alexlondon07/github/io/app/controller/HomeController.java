ad6600e1a3457b781d8d3d688571f26efb8d4e67
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-04-07 16:45:04 -0500

    Agregar objectos al modelo (Model)

--- src/main/java/alexlondon07/github/io/app/controller/HomeController.java
+++ src/main/java/alexlondon07/github/io/app/controller/HomeController.java
@@ -1,6 +1,7 @@
 package alexlondon07.github.io.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +11,20 @@ public class HomeController {
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String goHome() {
 		return "home";
+	}
+	
+	@RequestMapping(value="/detail")
+	public String showDetail(Model model) {
+		String title = "Rapidos y furiosos";
+		int duration = 136;
+		double price = 50;
+		
+		model.addAttribute("title", title);
+		model.addAttribute("duration", duration);
+		model.addAttribute("price", price);
+		
+		return "detail";
+		
 	}
 
 }
